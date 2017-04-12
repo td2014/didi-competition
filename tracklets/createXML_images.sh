24b9447a8925350b0f7c6ceb10b23d16d1aea45e
td2014 <anthonyd2004@gmail.com>
2017-04-12 13:31:20 -0700

    Added helper scripts.

---
+++ tracklets/createXML_images.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+# Simple script to create tracklet XML files for Didi contest
+#
+inputDir=/Users/anthonydaniell/Desktop/FilesToSync/Research/Didi_Udacity_Car_Challenge/Round1_Data/Didi-Release-2/Data/1
+outputDir=/Users/anthonydaniell/Desktop/FilesToSync/Research/Didi_Udacity_Car_Challenge/Round1_output/data_1
+
+./run-bag_to_kitti.sh -i $inputDir -o $outputDir
+
+#
+# End of script
+#
